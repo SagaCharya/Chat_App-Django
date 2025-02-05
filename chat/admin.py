d8298ac0381cb3ee62c3bc55b8da7ab5b5740d98
Saga Charya <sagacharya77@gmail.com>
2025-02-05 23:02:50 +0545

    Removed ignored files from Git tracking

--- chat/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, FriendRequest, Message
-
-# Register your models here.
-
-admin.site.register(UserProfile)
-admin.site.register(FriendRequest)
-admin.site.register(Message)
--- chat/context_processors.py
+++
@@ -1,6 +0,0 @@
-from .views import recent_chats
-
-def chat_sidebar(request):
-    if request.user.is_authenticated:
-        return recent_chats(request)
-    return {}
